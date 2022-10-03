6f96275739b9ab0049330b4fa8fe98ae581688d2
lucas <lucaslima12345.41@gmail.com>
2022-10-03 18:52:44 -0300

    criação das bibliotecas báscias

--- main.c
+++ main.c
@@ -1,45 +1,10 @@
 #include "raylib.h"
 #include <math.h>
+#include "car.h"
+#include "player.h"
 
 #define HEADING_LEFT 1
 #define HEADING_RIGHT 2
-
-typedef struct{
-    int posX;
-    int posY;
-    int frameCounter;
-    int life;
-    Rectangle playerCollision;
-    Rectangle playerLife;
-    int speed;
-} Player;
-
-typedef struct{
-    int posX;
-    int posY;
-    Rectangle carCollision;
-    Rectangle carLife;
-    float angle;
-    int speed;
-    int life;
-    float timeChangingAngle;
-} Car;
-
-void changeCarAngle(Car *car, Player player){
-    if((player.posX-car->posX)==0){
-        if(player.posY>car->posY){
-            car[0].angle = 90;
-        }
-        else if(player.posY<car->posY){
-            car[0].angle = 270;
-        }
-    }
-    else{
-
-        car[0].angle = atan((player.posY-car->posY)/(player.posX-car->posX))*180/PI;
-    }
-}
-
 
 int main()
 {
@@ -60,6 +25,7 @@ int main()
     playerX = 200;
     playerY = 200;
     float angle = 0;
+    
     
 
 
